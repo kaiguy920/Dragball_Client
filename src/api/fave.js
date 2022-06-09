24cbe80c5f6abd49e3234e10e27bebd6645868f2
kaiguy920 <smithkai920@gmail.com>
2022-06-09 14:59:09 -0400

    adds fave routes

--- src/api/fave.js
+++ src/api/fave.js
@@ -1,3 +1,26 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// INDEX function
+export const getAllFaves = (userId) => {
+    return axios(`${apiUrl}/dragball/myfaves/${userId}`)
+}
+
+// ADD to team
+// POST
+export const createTeamMember = (user, newTeamMember, queenId) => {
+    console.log('user', user)
+    console.log('this is queen added to team', newTeamMember)
+    return axios({
+        url: `${apiUrl}/dragball/myteam/${queenId}`,
+        method: 'POST',
+        headers: {
+            Authorization: `Token token=${user.token}`
+        },
+        data: { queen: newTeamMember }
+    })
+}
+
 // DELETE -> remove function
 export const removeQueen = (user, queenId) => {
     console.log('user', user)
