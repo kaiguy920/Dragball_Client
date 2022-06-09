2dde5a8b325ab8a20e463b768bfb68c22865ed36
kaiguy920 <smithkai920@gmail.com>
2022-06-09 15:04:04 -0400

    adds team routes

---
+++ src/api/team.js
@@ -0,0 +1,20 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// INDEX function
+export const getAllTeamMembers = (userId) => {
+    return axios(`${apiUrl}/dragball/myteam/${userId}`)
+}
+
+
+// DELETE -> remove function
+export const removeTeamMember = (user, queenId) => {
+    console.log('user', user)
+    return axios({
+        url: `${apiUrl}/dragball/myfaves/${queenId}`,
+        method: 'DELETE',
+        headers: {
+            Authorization: `Token token=${user.token}`
+        }
+    })
+}
