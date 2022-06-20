f579be132efcc9d01e5823decbf2779109f3fca7
kaiguy920 <smithkai920@gmail.com>
2022-06-20 14:01:59 -0400

    adds refine by functions

---
+++ src/api/refineindex.js
@@ -0,0 +1,17 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// index by season 
+export const getQueensBySeason = () => {
+    return axios(`${apiUrl}/dragball/season/${season}`)
+}
+
+// index by winners
+export const getAllWinners = () => {
+    return axios(`${apiUrl}/dragball/winners`)
+}
+
+// index by miss congeniality
+export const getAllCongenialities = () => {
+    return axios(`${apiUrl}/dragball/congeniality`)
+}
